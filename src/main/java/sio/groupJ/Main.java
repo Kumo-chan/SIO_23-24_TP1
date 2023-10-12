d803a368355ca7b722bd93c852e866cf2ac17397
Kumo-chan <patrick.furrer@heig-vd.ch>
2023-10-12 13:45:59 +0200

    Init

---
+++ src/main/java/sio/groupJ/Main.java
@@ -0,0 +1,26 @@
+package sio.groupJ;
+
+public final class Main {
+  public static void main(String[] args) {
+
+    System.out.println("Hello");
+    // TODO
+    //  - Renommage du package ;
+    //  - Implémentation des classes NearestNeighbor et DoubleEndsNearestNeighbor ;
+    //  - Affichage des statistiques dans la classe Main ;
+    //  - Documentation abondante des classes comprenant :
+    //    - la javadoc, avec auteurs et description des implémentations ;
+    //    - des commentaires sur les différentes parties de vos algorithmes.
+
+    // Longueurs optimales :
+    // att532 : 86729
+    // rat575 : 6773
+    // rl1889 : 316536
+    // u574   : 36905
+    // u1817  : 57201
+    // vm1748 : 336556
+
+    // Exemple de lecture d'un jeu de données :
+    // TspData data = TspData.fromFile("data/att532.dat");
+  }
+}
---
+++ src/main/java/sio/groupJ/NearestNeighbor.java
@@ -0,0 +1,13 @@
+package sio.groupJ;
+
+import sio.tsp.TspData;
+import sio.tsp.TspConstructiveHeuristic;
+import sio.tsp.TspTour;
+
+public final class NearestNeighbor implements TspConstructiveHeuristic {
+  @Override
+  public TspTour computeTour(TspData data, int startCityIndex) {
+    // TODO
+    return null;
+  }
+}
